76bae015e3faeaa4cf6c71f4ee636597e94a8057
Jake Glassman & Vaughn Dice <student@epicodus.com>
2015-09-28 11:50:30 -0700

    add update-listing component

---
+++ app/components/update-listing.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  showUpdateListing: false,
+  actions: {
+    updateListingShow() {
+      this.set('showUpdateListing', true);
+    },
+    updateListingHide() {
+      this.set('showUpdateListing', false);
+    },
+    updateListing(listing) {
+      var params = {
+        title: this.get('title'),
+        description: this.get('description'),
+        category: this.get('category'),
+        date: new Date()
+      };
+      this.sendAction('updateListing', listing, params);
+      this.set('showUpdateListing', false);
+    }
+  }
+});
--- app/routes/listing.js
+++ app/routes/listing.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('listing', params.listing_id);
+  },
+  actions: {
+    updateListing(listing, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          listing.set(key,params[key]);
+        }
+      });
+
+      listing.save()
+    }
   }
 });
